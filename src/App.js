201aaf384ae5b7e8c7276a860ecd075242120a75
Everton Ferreira <ferreira.everton@icloud.com>
2020-11-28 00:10:48 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import LessonPlan from './pages/LessonPlan';
+
+import Header from './components/Header'
+
+const App = () => (
+
+  <Router>
+
+    <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/catalog">
+              <Catalog />
+            </Route>
+            <Route path="/lessonplan">
+              <LessonPlan />
+            </Route>
+          </Switch>
+
+  </Router>
+);
 
 export default App;
+
+
+/*
+
+
+          
+          <ButtonToolbar className="custom-btn-toolbar">
+            <LinkContainer to="/">
+              <Button>Home</Button>
+            </LinkContainer>
+            <LinkContainer to="/catalog">
+              <Button>About</Button>
+            </LinkContainer>
+            <LinkContainer to="/lessonplan">
+              <Button>Users</Button>
+            </LinkContainer>
+          </ButtonToolbar>
+
+
+*/
